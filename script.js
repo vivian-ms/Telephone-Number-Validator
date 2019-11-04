44a73821bfc8414b227596ac4b046a7294b79676
vivian-ms <56147408+vivian-ms@users.noreply.github.com>
2019-11-03 17:09:59 -0800

    Added UI for user to enter string to check if it's a valid US telephone number

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+$(function() {
+  $('form').on('submit', function(evt) {
+    evt.preventDefault();
+    telephoneCheck( $('input').val() );
+  });
+
+  $('#clear').on('click', function(evt) {
+    $('#result').empty();
+    $('input').focus();
+  });
+});
+
 function telephoneCheck(str) {
   /* 1. Country code: (1(-|\s)?)?  -->  country code (1) is optional; - or whitespace after 1 is optional
       2. Area code: (\d{3}|(\(\d{3}\)))  -->  three digits or three digits within parentheses
@@ -8,8 +20,10 @@ function telephoneCheck(str) {
   let regex = /^(1(-|\s)?)?(\d{3}|(\(\d{3}\)))(-|\s)?\d{3}(-|\s)?\d{4}$/;
 
   if (regex.test(str)) {
-    return true;
+    $('#result').append(`<li><strong>${str}</strong> is a valid US phone number</li>`);
   } else {
-    return false;
+    $('#result').append(`<li><strong>${str}</strong> is not a valid US phone number</li>`);
   }
+
+  $('input').val('');
 }
